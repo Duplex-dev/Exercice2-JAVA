11a73661a3edd8219b6cc6af885fae32f5c15155
Duplex-dev <72447371+Duplex-dev@users.noreply.github.com>
2020-11-17 23:52:21 +0100

    Add files via upload

---
+++ Exercice JAVA 2/Exercice2/JS/main.js
@@ -0,0 +1,15 @@
+// function changer_style(){
+//     let p = document.getElementById('parag1');
+//     p.style.color="white";
+//     p.style.backgroundColor="#bbb";
+//     p.style.border="1px solid gray";
+//     p.style.padding="5px";}
+
+
+
+    function changer_style(){
+
+        let p = document.getElementById('parag1');
+        p.classList.add("active");
+
+      }
